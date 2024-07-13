5f387e53bad352d3bf794d0c887b5033daa90dd8
ben12mwaniki <ben12mwaniki@gmail.com>
2024-07-13 17:00:35 -0400

    added endpoint create_user

--- skillup/djangoapp/urls.py
+++ skillup/djangoapp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-
 from . import views
 
-urlpatterns = [path('',views.index,name='index'),]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create_user',views.create_user,name='create_user'),
+    path('create_resource',views.create_resource,name='create_resource'),
+
+    ]
--- skillup/djangoapp/views.py
+++ skillup/djangoapp/views.py
@@ -2,27 +2,74 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pymongo
 
+client = pymongo.MongoClient('mongodb+srv://hurrivan:pCowJ3o9lke1fRIO@cluster0.fq0iqud.mongodb.net/')
+
+
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello and welcome to <u>Skill UP</u> project!</h1>")
 
-client = pymongo.MongoClient('mongodb+srv://hurrivan:pCowJ3o9lke1fRIO@cluster0.fq0iqud.mongodb.net/')
+def create_user(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        dbname = client['skillupdb']
+        collection = dbname['users']
+        user = {
+            "name": name,
+            "email": email,
+            "password": password
+        }
+        collection.insert_one(user)
+        return HttpResponse("<h1>User created successfully!</h1>")
 
-#Define Db Name
-dbname = client['skillupdb']
+def create_resource(request):
+    if request.method == 'POST':
+        type = request.POST['type']
+        link = request.POST['link']
+        subject = request.POST['subject']
 
-#Define Collection
-collection = dbname['resources']
+        dbname = client['skillupdb']
+        collection = dbname['resources']
+        resource = {
+            "subject": subject,
+            "type": type,
+            "link": link
+        }
+        collection.insert_one(resource)
+        return HttpResponse("<h1>Resource created successfully!</h1>")
+    
 
-resource_1={
-    "name": "python_intro",
-    "type" : "video",
-    "link" : "https://www.youtube.com/watch?v=rfscVS0vtbw"
-}
 
-collection.insert_one(resource_1)
+def get_users(request):
+    dbname = client['skillupdb']
+    collection = dbname['users']
+    users = collection.find()
+    
+    for user in users:
+        print(user)     
+    return HttpResponse("<h1>Users fetched successfully!</h1>")
 
-resources_details = collection.find({})
+def get_resources(request):
+    dbname = client['skillupdb']
+    collection = dbname['resources']
+    resources = collection.find({'type': "video"})
+    
+    for resource in resources:
+        print(resource)     
+    return HttpResponse("<h1>Resources fetched successfully!</h1>")
 
-for r in resources_details:
-    print(r['name'])
+
+get_resources(None) 
+
+
+
+
+
+
+
+
+
+
+
