45c9cd97a1ddb9799cad4a56ba3168704cd2f2e7
ben12mwaniki <ben12mwaniki@gmail.com>
2024-08-02 19:21:53 -0400

    implement rate resource, comment, save resource, add navbar

--- skillup/djangoapp/templatetags/mongofilters.py
+++ skillup/djangoapp/templatetags/mongofilters.py
@@ -4,3 +4,11 @@ register = template.Library()
 @register.filter(name='mongo_id')
 def mongo_id(resource):
     return str(resource['_id'])
+
+@register.filter(name='round')
+def round_val(value):
+    if value == '' or value is None:
+        return 0 
+    if type(value) != float:
+        int(value)
+    return round(value)
